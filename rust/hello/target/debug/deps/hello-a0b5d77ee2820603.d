ed028e7b622e26e717999f597bab7d05714fd33c
Paul Blair <blairpaul8@yahoo.com>
2026-01-21 08:37:34 -0500

    Added folders for each language

---
+++ rust/hello/target/debug/deps/hello-a0b5d77ee2820603.d
@@ -0,0 +1,5 @@
+/home/charlie/Documents/01_UTK_spring_2026/cs365/rust/hello/target/debug/deps/hello-a0b5d77ee2820603.d: src/main.rs
+
+/home/charlie/Documents/01_UTK_spring_2026/cs365/rust/hello/target/debug/deps/hello-a0b5d77ee2820603: src/main.rs
+
+src/main.rs:
---
+++ rust/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/charlie/Documents/01_UTK_spring_2026/cs365/rust/hello/target/debug/hello: /home/charlie/Documents/01_UTK_spring_2026/cs365/rust/hello/src/main.rs
